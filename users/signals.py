031cc7e88bf2b53ee288c68e5eae043a2c2ac75e
Igor Czernichowski <88770621+Czernich@users.noreply.github.com>
2022-02-25 14:15:03 +0100

    Sending emails and password reset

--- users/signals.py
+++ users/signals.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
+from django.core.mail import send_mail
+from django.conf import settings
+
 @receiver(post_save, sender=User)
 def createProfile(sender, instance, created, **kwargs):
     if created:
@@ -13,6 +16,18 @@ def createProfile(sender, instance, created, **kwargs):
             email = user.email,
             name = user.first_name
         )
+
+        subject = "Welcome to DecSearch"
+        message = "We are glad you are here!"
+
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False
+        )
+
 
 @receiver(post_save, sender=Profile)
 def updateUser(sender, instance, created, **kwargs):
