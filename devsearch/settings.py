b1075487884594848aa1b23315ef0ad35e9fd1fc
Igor Czernichowski <88770621+Czernich@users.noreply.github.com>
2022-03-06 00:38:49 +0100

    Preparing for deploying a project - last commit

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -123,10 +123,21 @@ WSGI_APPLICATION = 'devsearch.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'devsearch',
+        'USER': 'postgres',
+        'PASSWORD': 'PASSWORD',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
